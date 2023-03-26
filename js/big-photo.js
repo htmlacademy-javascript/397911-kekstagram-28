e2107c90f4ddc4aca7da8ef4338e85ea6d77cd4e
Rogova Natalia <kamelia-17@mail.ru>
2023-03-26 19:15:41 +0200

    Вносит исправления

--- js/big-photo.js
+++ js/big-photo.js
@@ -3,11 +3,8 @@ import {renderComments} from './comments.js';
 const bigPicture = document.querySelector('.big-picture');
 const likesCount = bigPicture.querySelector('.likes-count');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
-const commentsCountContainer = bigPicture.querySelector('.social__comment-count');
-const commentsCount = commentsCountContainer.querySelector('.comments-count');
 const cancelBtn = document.querySelector('.big-picture__cancel');
 const description = bigPicture.querySelector('.social__caption');
-const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 
 cancelBtn.addEventListener('click',() => {
@@ -27,12 +24,9 @@ export const showBigPhoto = (photo) => {
   bigPictureImg.setAttribute('src', photo.url);
   likesCount.textContent = photo.likes;
   description.textContent = photo.description;
-  commentsCount.textContent = photo.comments.length;
 
   renderComments(photo.comments);
 
-  commentsCountContainer.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   document.body.classList.add('modal-open');
   bigPicture.classList.remove('hidden');
 };
