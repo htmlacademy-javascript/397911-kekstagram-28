789f42ecd1f7bda0400438874c05d84f28664e76
Rogova Natalia <kamelia-17@mail.ru>
2023-04-02 22:31:43 +0200

    Добавил обработку ошибок загрузки данных с сервера

--- js/server-data.js
+++ js/server-data.js
@@ -1,7 +1,28 @@
 import { insertPhotos } from './photos.js';
 
+const ERROR_SHOW_TIME = 5000;
+
+const errorTemplate = document.querySelector('#fetch-error')
+  .content
+  .querySelector('.fetch-error');
+
 fetch('https://28.javascript.pages.academy/kekstagram/data')
+  .then((res) => {
+    if (res.status !== 200) {
+      throw new Error(`Response status ${res.status}`);
+    }
+    return res;
+  })
   .then((res) => res.json())
   .then((json) => {
     insertPhotos(json);
+  })
+  .catch((e) => {
+    const errorElement = errorTemplate.cloneNode(true);
+    errorElement.innerText = e.message;
+    document.body.appendChild(errorElement);
+
+    setTimeout(() => {
+      errorElement.remove();
+    }, ERROR_SHOW_TIME);
   });
